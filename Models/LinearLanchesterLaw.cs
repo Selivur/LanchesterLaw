fa67d75d5373b7131dfa9250540b5de8e55b3eb8
Illia <si01082001@gmail.com>
2023-05-09 23:34:48 +0300

    make working LinearLanchesterLaw Page

--- Models/LinearLanchesterLaw.cs
+++ Models/LinearLanchesterLaw.cs
@@ -8,9 +8,21 @@ namespace LanchesterLaw.Models
 {
     internal class LinearLanchesterLaw
     {
-        public LinearLanchesterLaw()
+        public int[] AllyCount { get; }
+        public int[] EnemyCount { get; }
+        public LinearLanchesterLaw(uint ally, uint enemy, uint ff, uint landscape, uint weather)//TODO add fortfact+learn about k5
         {
-
+            AllyCount = new int[50];
+            EnemyCount = new int[50];
+            AllyCount[0] = (int)ally;
+            EnemyCount[0] = (int)enemy;            
+            for (int i = 1;i < 50; i++)
+            {
+                double AllyCoef = 0.02 * landscape * 0.1 * weather * 0.1 * 0.8;
+                double EnemyCoef = 0.02 * landscape * 0.1 * weather * 0.1;
+                AllyCount[i] = (AllyCount[i - 1] - (int)(Convert.ToDouble(EnemyCount[i - 1]) * AllyCoef)) >= 0 ? ((AllyCount[i - 1] - (int)(Convert.ToDouble(EnemyCount[i - 1]) * AllyCoef))) : (0);
+                EnemyCount[i] = (EnemyCount[i - 1] - (int)(Convert.ToDouble(AllyCount[i - 1]) * EnemyCoef)) >= 0 ? (EnemyCount[i - 1] - (int)(Convert.ToDouble(AllyCount[i - 1]) * EnemyCoef)) : (0);
+            }
         }
     }
 }
--- ViewModels/LinearLanchesterLawViewModel.cs
+++ ViewModels/LinearLanchesterLawViewModel.cs
@@ -1,54 +1,126 @@
-﻿using LiveCharts;
+﻿using LanchesterLaw.Commands;
+using LanchesterLaw.Models;
+using LanchesterLaw.Stores;
+using LiveCharts;
 using LiveCharts.Wpf;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 using System.Windows.Media;
 
 namespace LanchesterLaw.ViewModels
 {
     internal class LinearLanchesterLawViewModel : ViewModelBase
     {
-        public SeriesCollection SeriesCollection { get; set; }
+        private uint _enemyCount = 0;
+        public uint EnemyCount
+        {
+            get => _enemyCount;
+            set
+            {
+                _enemyCount = value;
+                OnPropertyChanged();
+            }
+        }
+        private uint _allyCount = 0;
+        public uint AllyCount
+        {
+            get => _allyCount;
+            set
+            {
+                _allyCount = value;
+                OnPropertyChanged();
+            }
+        }
+        private uint _fortificationFactor = 1;
+        public uint FortificationFactor
+        {
+            get => _fortificationFactor;
+            set
+            {
+                _fortificationFactor = value;
+                OnPropertyChanged();
+            }
+        }
+        private uint _landscapeFactor=1;
+        public uint LandscapeFactor
+        {
+            get => _landscapeFactor;
+            set
+            {
+                _landscapeFactor = value;
+                OnPropertyChanged();
+            }
+        }
+        private uint _weatherFactor = 1;
+        public uint WeatherFactor
+        {
+            get => _weatherFactor;
+            set
+            {
+                _weatherFactor = value;
+                OnPropertyChanged();
+            }
+        }
+        public SeriesCollection _seriesCollection;
+        public SeriesCollection SeriesCollection
+        {
+            get => _seriesCollection;
+            set
+            {
+                _seriesCollection = value;
+                OnPropertyChanged();
+            }
+        }
         public string[] Labels { get; set; }
         public Func<double, string> YFormatter { get; set; }
         public LinearLanchesterLawViewModel()
         {
-            SeriesCollection = new SeriesCollection
-            {
-                new LineSeries
-                {
-                    Title = "Series 1",
-                    Values = new ChartValues<double> { 4, 6, 5, 2 ,4 }
-                },
-                new LineSeries
-                {
-                    Title = "Series 2",
-                    Values = new ChartValues<double> { 6, 7, 3, 4 ,6 },
-                    PointGeometry = null
-                },
-                new LineSeries
-                {
-                    Title = "Series 3",
-                    Values = new ChartValues<double> { 4,2,7,2,7 },
-                    PointGeometry = DefaultGeometries.Square,
-                    PointGeometrySize = 15
-                }
-            };
-            Labels = new[] { "Jan", "Feb", "Mar", "Apr", "May" };
-            YFormatter = value => value.ToString("C");
-            //SeriesCollection.Add(new LineSeries
+            //SeriesCollection = new SeriesCollection
             //{
-            //    Title = "Series 4",
-            //    Values = new ChartValues<double> { 5, 3, 2, 4 },
-            //    LineSmoothness = 0, //0: straight lines, 1: really smooth lines
-            //    PointGeometry = Geometry.Parse("m 25 70.36218 20 -28 -20 22 -8 -6 z"),
-            //    PointGeometrySize = 50,
-            //    PointForeground = Brushes.Gray
-            //});
-            //SeriesCollection[3].Values.Add(5d);
+            //    new LineSeries
+            //    {
+            //        Title = "Оборона",
+            //        Values = new ChartValues<double> { 4, 6, 5, 2 , 4 }
+            //    },
+            //    new LineSeries
+            //    {
+            //        Title = "Атака",
+            //        Values = new ChartValues<double> { 6, 7, 3, 4 ,6 },
+            //        PointGeometry = null
+            //    }
+            //};            
+        }
+        private RelayCommand _calculateCommand;
+        public RelayCommand CalculateCommand
+        {
+            get
+            {
+                return _calculateCommand ?? new RelayCommand(obj =>
+                {
+                    var Lanchester = new LinearLanchesterLaw(_allyCount, _enemyCount, _fortificationFactor, _landscapeFactor, _weatherFactor);
+                    /////////
+                    SeriesCollection = new SeriesCollection()
+                    {
+                        
+                        new LineSeries
+                        {
+                            Title = "Оборона",
+                            Values = new ChartValues<int>(Lanchester.AllyCount)
+                        },
+                        new LineSeries
+                        {
+                            Title = "Атака",
+                            Values = new ChartValues<int> (Lanchester.EnemyCount)
+                        },
+                    };
+                    ///
+                    MessageBox.Show(_fortificationFactor.ToString()+_landscapeFactor.ToString() +_weatherFactor.ToString());
+                });
+            }
         }
     }
 }
