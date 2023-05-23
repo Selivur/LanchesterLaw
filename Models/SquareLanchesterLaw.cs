2dc0a549fd808c113381ef4e70f18982aa52d18a
Illia <si01082001@gmail.com>
2023-05-24 01:26:28 +0300

    make working SquareLanchesterLaw Page

--- Models/SquareLanchesterLaw.cs
+++ Models/SquareLanchesterLaw.cs
@@ -6,21 +6,21 @@ using System.Threading.Tasks;
 
 namespace LanchesterLaw.Models
 {
-    internal class LinearLanchesterLaw
+    internal class SquareLanchesterLaw
     {
         public int[] AllyCount { get; }
         public int[] EnemyCount { get; }
-        private int _length=100;
-        public LinearLanchesterLaw(uint ally, uint enemy, uint ff, uint landscape, uint weather)
+        private int _length = 50;
+        public SquareLanchesterLaw(uint allyCount, uint allyUnitSquare, uint allySquare, uint allyPower, uint enemyCount, uint enemyUnitSquare, uint enemySquare, uint enemyPower)
         {
             AllyCount = new int[_length];
             EnemyCount = new int[_length];
-            AllyCount[0] = (int)ally;
-            EnemyCount[0] = (int)enemy;            
-            for (int i = 1;i < _length; i++)
+            AllyCount[0] = (int)allyCount;
+            EnemyCount[0] = (int)enemyCount;
+            for (int i = 1; i < _length; i++)
             {
-                double AllyCoef = 0.02 * landscape * 0.1 * weather * 0.1 * 0.8 * ff * 0.1;
-                double EnemyCoef = 0.02 * landscape * 0.1 * weather * 0.1;
+                double AllyCoef = (double)allyPower* (double)allyUnitSquare / (double)allySquare;
+                double EnemyCoef = (double)enemyPower * (double)enemyUnitSquare / (double)enemySquare;
                 AllyCount[i] = (AllyCount[i - 1] - (int)(Convert.ToDouble(EnemyCount[i - 1]) * AllyCoef)) >= 0 ? ((AllyCount[i - 1] - (int)(Convert.ToDouble(EnemyCount[i - 1]) * AllyCoef))) : (0);
                 EnemyCount[i] = (EnemyCount[i - 1] - (int)(Convert.ToDouble(AllyCount[i - 1]) * EnemyCoef)) >= 0 ? (EnemyCount[i - 1] - (int)(Convert.ToDouble(AllyCount[i - 1]) * EnemyCoef)) : (0);
             }
