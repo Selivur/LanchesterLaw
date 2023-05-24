9037aa5c09739d6f68587f9cd12e18c0d11a0e4e
Illia <si01082001@gmail.com>
2023-05-24 21:12:32 +0300

    add working UniversalLanchesterlaw Page

--- Models/UniversalLanchesterLaw.cs
+++ Models/UniversalLanchesterLaw.cs
@@ -6,12 +6,12 @@ using System.Threading.Tasks;
 
 namespace LanchesterLaw.Models
 {
-    internal class SquareLanchesterLaw
+    internal class UniversalLanchesterLaw
     {
         public int[] AllyCount { get; }
         public int[] EnemyCount { get; }
         private int _length = 100;
-        public SquareLanchesterLaw(uint allyCount, uint allyUnitSquare, uint allySquare, uint allyPower, uint enemyCount, uint enemyUnitSquare, uint enemySquare, uint enemyPower)
+        public UniversalLanchesterLaw(uint allyCount, uint allyUnitSquare, uint allySquare, uint allyPower, uint enemyCount, uint enemyPower)
         {
             AllyCount = new int[_length];
             EnemyCount = new int[_length];
@@ -19,10 +19,10 @@ namespace LanchesterLaw.Models
             EnemyCount[0] = (int)enemyCount;
             for (int i = 1; i < _length; i++)
             {
-                double AllyCoef = (double)enemyPower* (double)allyUnitSquare / (double)allySquare;
-                double EnemyCoef = (double)allyPower * (double)enemyUnitSquare / (double)enemySquare;
+                double AllyCoef = (double)enemyPower * (double)allyUnitSquare / (double)allySquare;
+                double EnemyCoef = (double)allyPower * 0.02;
                 AllyCount[i] = (AllyCount[i - 1] - (int)(Convert.ToDouble(EnemyCount[i - 1]) * AllyCoef * Convert.ToDouble(AllyCount[i - 1]))) >= 0 ? ((AllyCount[i - 1] - (int)(Convert.ToDouble(EnemyCount[i - 1]) * AllyCoef * Convert.ToDouble(AllyCount[i - 1])))) : (0);
-                EnemyCount[i] = (EnemyCount[i - 1] - (int)(Convert.ToDouble(AllyCount[i - 1]) * EnemyCoef * Convert.ToDouble(EnemyCount[i - 1]))) >= 0 ? (EnemyCount[i - 1] - (int)(Convert.ToDouble(AllyCount[i - 1]) * EnemyCoef * Convert.ToDouble(EnemyCount[i - 1]))) : (0);
+                EnemyCount[i] = (EnemyCount[i - 1] - (int)(Convert.ToDouble(AllyCount[i - 1]) * EnemyCoef)) >= 0 ? (EnemyCount[i - 1] - (int)(Convert.ToDouble(AllyCount[i - 1]) * EnemyCoef)) : (0);
             }
         }
     }
